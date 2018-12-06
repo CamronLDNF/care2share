36d27b9ad52d885702aa4d59c0a4e16d7b04f143
camron <camronmoradi@gmail.com>
2018-12-06 23:37:48 +0100

    Revises campaign show view and donation new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'donations/new'
-  get 'donations/create'
+  resources :donations, only: [:new, :create]
   resources :campaigns, only: [:index, :new, :create, :show]
   # resources :donations, only: [:new, :create]
   root controller: :campaigns, action: :index
